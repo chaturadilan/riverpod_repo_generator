0840d32b87f747d9e2ff7339e0a145aa2d33799f
Chatura Dilan Perera <dilan@dilan.me>
2023-03-22 16:24:43 +0530

    fixed the READMEs

--- lib/src/riverpod_repo_generator.dart
+++ lib/src/riverpod_repo_generator.dart
@@ -7,10 +7,8 @@
 
 import 'dart:core';
 
-// ignore_for_file: implementation_imports, depend_on_referenced_packages
-
 import 'package:analyzer/dart/element/element.dart';
-import 'package:build/src/builder/build_step.dart';
+import 'package:build/build.dart';
 import 'package:recase/recase.dart';
 import 'package:source_gen/source_gen.dart';
 
